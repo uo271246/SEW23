c7adbdbcba7f37b14fd413d5818b1747878e9b2c
uo271246 <78324143+uo271246@users.noreply.github.com>
2023-06-14 02:23:05 +0200

    Add files via upload

--- php/baseDeDatos.php
+++ php/baseDeDatos.php
@@ -34,7 +34,6 @@ class BaseDeDatos
         $this->conexion->query($crearBdSQL); } 
         else {
             $resultado = mysqli_query($conexion, $crearBdSQL);
-
         }
     }
 
@@ -44,25 +43,27 @@ class BaseDeDatos
     }
 
     public function query($sql)
-    {
+    {   if ($_SERVER['HTTP_HOST'] === 'localhost') {
         return $this->conexion->query($sql);
+    } else {
+        mysqli_query($conexion, $sql);
+    }
     }
 
     private function crearTablas()
-    {
+    { 
         $crearTablaUsuarios = "CREATE TABLE IF NOT EXISTS users (
             id INT AUTO_INCREMENT PRIMARY KEY,
             email VARCHAR(50),
             password VARCHAR(50)
         )";
-        $this->conexion->query($crearTablaUsuarios);
         
         $crearTablaCategories = "CREATE TABLE IF NOT EXISTS categories (
             id INT AUTO_INCREMENT PRIMARY KEY,
             name VARCHAR(150),
             description VARCHAR(150)
         )";
-        $this->conexion->query($crearTablaCategories);
+       
         $crearTablaRecursos = "CREATE TABLE IF NOT EXISTS resources (
             id INT AUTO_INCREMENT PRIMARY KEY,
             name VARCHAR(150),
@@ -72,7 +73,7 @@ class BaseDeDatos
             price DECIMAL(10,2),
             FOREIGN KEY (category_id) REFERENCES categories (id)
         )";
-        $this->conexion->query($crearTablaRecursos);
+       
         $crearTablaBookings = "CREATE TABLE IF NOT EXISTS bookings (
             id INT AUTO_INCREMENT PRIMARY KEY,
             user_id INT,
@@ -82,35 +83,64 @@ class BaseDeDatos
             FOREIGN KEY (user_id) REFERENCES users (id),
             FOREIGN KEY (resource_id) REFERENCES resources (id)
         );";
-        $this->conexion->query($crearTablaBookings);
+        
         $crearTablaBills = "CREATE TABLE IF NOT EXISTS bills (
             id INT AUTO_INCREMENT PRIMARY KEY,
             user_id INT,
             total DECIMAL(10,2),
             FOREIGN KEY (user_id) REFERENCES users (id)
         );";
-        $this->conexion->query($crearTablaBills);
+         if ($_SERVER['HTTP_HOST'] === 'localhost') {
+            $this->conexion->query($crearTablaUsuarios);
+            $this->conexion->query($crearTablaCategories);
+            $this->conexion->query($crearTablaRecursos);
+            $this->conexion->query($crearTablaBookings);    
+            $this->conexion->query($crearTablaBills);
+    
+    } else {
+        mysqli_query($conexion,$crearTablaUsuarios);
+        mysqli_query($conexion,$crearTablaCategories);
+        mysqli_query($conexion,$crearTablaRecursos);
+        mysqli_query($conexion,$crearTablaBookings);    
+        mysqli_query($conexion,$crearTablaBills);
+
+
+    }
     }
 
     private function crearDatos()
         //TABLA CATEGORIES
     {   //Id 1
         $crearCategoria = "INSERT IGNORE INTO categories (id, name, description) VALUES (1,'Senderismo', 'Ruta de senderismo')";
-        $this->conexion->query($crearCategoria);
+        
         //Id 2
         $crearCategoria = "INSERT IGNORE INTO categories (id, name, description) VALUES (2,'Restaurante', 'Ir a un restaurante')";
-        $this->conexion->query($crearCategoria);
+        
         //Id 3
         $crearCategoria = "INSERT IGNORE INTO categories (id, name, description) VALUES (3, 'Hotel', 'Ir a un hotel')";
-        $this->conexion->query($crearCategoria);
+        
         //Id 4
         $crearCategoria = "INSERT IGNORE INTO categories (id, name, description) VALUES (4, 'Museo', 'Ir a un museo')";
-        $this->conexion->query($crearCategoria);
+        
         //TABLA RESOURCES
         //Id 1
         $crearRecurso = "INSERT IGNORE INTO resources (id, name, description, category_id, available, price) 
         VALUES (1,'Subida al Angliru', 'Ruta de subida hasta la cima del Angliru', 1, 5, 10)";
-        $this->conexion->query($crearRecurso);
+        
+        if($_SERVER['HTTP_HOST'] === 'localhost') {
+            $this->conexion->query($crearCategoria);   
+            $this->conexion->query($crearCategoria);
+            $this->conexion->query($crearCategoria);
+            $this->conexion->query($crearCategoria);
+            $this->conexion->query($crearRecurso);
+        } else {
+            mysqli_query($conexion,$crearCategoria);   
+            mysqli_query($conexion,$crearCategoria);
+            mysqli_query($conexion,$crearCategoria);
+            mysqli_query($conexion,$crearCategoria);
+            mysqli_query($conexion,$crearRecurso);
+        }   
+
     }
 
     public function cerrarConexion()
